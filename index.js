b200d5b270756848fbab98d70f4081f19940b543
Lucas Rack <lucas_rack@live.com.ar>
2020-10-01 21:11:26 -0300

    login con key

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import { Command } from './modules/commands.js';
 
 import Discord from 'discord.js';
+import CryptoJS from 'crypto-js';
+import readline from 'readline';
 
 const client = new Discord.Client();
 let command = new Command();
 
-client.login('NzEzNTI0NTE5ODMwMDI4MzY4.XshdJg.5t2rry7WeOsgzPYDyX_wLDxxXLE');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.stdoutMuted = true;
+
+var encrypted = 'U2FsdGVkX19x/uMboX8ADZQLoRSP1dvKIY36FlGruFPP0ixaEVT3HUtMHBxPd2B69cIorl/bD4EKb9D4MZZ101g/O17Tj3aYHlQIVmiblC0=';
+
+rl.question('Enter Key\n', (key) => {
+   	rl.stdoutMuted = false;
+   	console.log("");
+   	var decrypted = CryptoJS.AES.decrypt(encrypted, key);
+   	client.login(decrypted.toString(CryptoJS.enc.Utf8));
+  	rl.close();
+});
+
+rl._writeToOutput = function _writeToOutput(stringToWrite) {
+  if (rl.stdoutMuted)
+    rl.output.write("*");
+  else
+    rl.output.write(stringToWrite);
+};
 
 client.on('message', msg => {
 	command.exec(msg, client);
