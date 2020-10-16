88113186332aad4705e22cf49ab7e1f259ad2853
Lucas Rack <lrack@fi.uba.ar>
2020-10-15 21:47:28 -0300

    fix token

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 import { Command } from './modules/commands.js';
 
 import Discord from 'discord.js';
-import CryptoJS from 'crypto-js';
-import readline from 'readline';
+import fs from 'fs';
 
 const client = new Discord.Client();
 let command = new Command();
-/*
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+
+//client.login("NzEzNTI0NTE5ODMwMDI4MzY4.XshXfg.Hjnuuf9e0xRFegSgK-EJDicxAw0");
+
+fs.readFile('./bin/config.json','utf-8',(err,jsonString)=>{
+    const config = JSON.parse(jsonString);
+    client.login(config.token);
 });
-
-rl.stdoutMuted = true;
-
-var encrypted =  'U2FsdGVkX19x/uMboX8ADZQLoRSP1dvKIY36FlGruFPP0ixaEVT3HUtMHBxPd2B69cIorl/bD4EKb9D4MZZ101g/O17Tj3aYHlQIVmiblC0=';
-
-rl.question('Enter Key\n', (key) => {
-   	rl.stdoutMuted = false;
-   	console.log();
-   	var decrypted = CryptoJS.AES.decrypt(encrypted, key);
-   	client.login(decrypted.toString(CryptoJS.enc.Utf8));
-  	rl.close();
-});
-
-rl._writeToOutput = function _writeToOutput(stringToWrite) {
-  if (rl.stdoutMuted)
-    rl.output.write("*");
-  else
-    rl.output.write(stringToWrite);
-};
-
-*/
-
-
-client.login("NzEzNTI0NTE5ODMwMDI4MzY4.XshXfg.Hjnuuf9e0xRFegSgK-EJDicxAw0");
-
 
 
 client.on('message', msg => {
